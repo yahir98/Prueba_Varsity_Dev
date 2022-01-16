b9ad4cd218082290d1c26a9021a5ca9e7bc6b32b
yahir98 <yahirisaacurbina@gmail.com>
2022-01-16 09:45:02 -0600

    creacion de backend y metodo de respuesta

---
+++ API_Twitter/API_Twitter/Models/Response/Respuesta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Twitter.Models.Response
+{
+    public class Respuesta
+    {
+
+        public int Exito { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public object Data { get; set; }
+
+        public Respuesta()
+        {
+            this.Exito = 0;
+        }
+    }
+}
